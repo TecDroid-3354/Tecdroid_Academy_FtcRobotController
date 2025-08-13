d599baf30e77647ad58c7ce19a8b0dbd0a7df7ab
Santiago Fernando Murguia <santiagofernandomurguia@gmail.com>
2025-08-13 17:42:21 -0600

    op mode added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
-public class OpMode {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp(name = "CMD", group = "Op mode")
+
+public class OpMode extends LinearOpMode {
+
+    public void initialize() {
+
+    }
+
+    @Override
+    public void runOpMode() {
+        initialize();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+        }
+    }
 }
